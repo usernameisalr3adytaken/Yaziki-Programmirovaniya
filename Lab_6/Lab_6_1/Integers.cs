8f82ca9d67421c686a4f39627fd771a8e5d42911
usernameisalr3adytaken <sl15so2004@gmail.com>
2025-04-09 11:53:18 +0500

    Update Integers.cs

--- Lab_6/Lab_6_1/Integers.cs
+++ Lab_6/Lab_6_1/Integers.cs
@@ -2,38 +2,38 @@
 
 class Integers
 {
-    private int aValue { get; set; }
-    private int bValue { get; set; }
-    private int cValue { get; set; }
+    private int _aValue { get; set; }
+    private int _bValue { get; set; }
+    private int _cValue { get; set; }
 
     public Integers()
     {
-        aValue = InputDataWithCheck.InputIntegerWithValidation($"Введите натуральное число A (от {int.MinValue} до {int.MaxValue})", int.MinValue, int.MaxValue);
-        bValue = InputDataWithCheck.InputIntegerWithValidation($"Введите натуральное число B (от {int.MinValue} до {int.MaxValue})", int.MinValue, int.MaxValue);
-        cValue = InputDataWithCheck.InputIntegerWithValidation($"Введите натуральное число C (от {int.MinValue} до {int.MaxValue})", int.MinValue, int.MaxValue);
+        _aValue = InputDataWithCheck.InputIntegerWithValidation($"Введите натуральное число A (от {int.MinValue} до {int.MaxValue})", int.MinValue, int.MaxValue);
+        _bValue = InputDataWithCheck.InputIntegerWithValidation($"Введите натуральное число B (от {int.MinValue} до {int.MaxValue})", int.MinValue, int.MaxValue);
+        _cValue = InputDataWithCheck.InputIntegerWithValidation($"Введите натуральное число C (от {int.MinValue} до {int.MaxValue})", int.MinValue, int.MaxValue);
 
     }
     public Integers(int a, int b, int c)
     {
-        aValue = a;
-        bValue = b;
-        cValue = c;
+        _aValue = a;
+        _bValue = b;
+        _cValue = c;
     }
     public Integers(Integers number)
     {
-        aValue = number.aValue;
-        bValue = number.bValue;
-        cValue = number.cValue;
+        _aValue = number._aValue;
+        _bValue = number._bValue;
+        _cValue = number._cValue;
     }
 
     public int Multiply()
     {
-        return aValue * bValue * cValue;
+        return _aValue * _bValue * _cValue;
     }
 
     public override string ToString()
     {
-        return $"Число А = {aValue}" + $", число В = {bValue}" + $", число С = {cValue}";
+        return $"Число А = {_aValue}" + $", число В = {_bValue}" + $", число С = {_cValue}";
     }
 
 }
